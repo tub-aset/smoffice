d393f1d98a61ac7560259f9db3c8cd952dffd61e
Andreas Vogelsang <andreas.vogelsang@tu-berlin.de>
2019-11-22 11:03:14 +0100

    first working server

--- jaxrs-server/src/main/java/io/swagger/api/impl/SensordataApiServiceImpl.java
+++ jaxrs-server/src/main/java/io/swagger/api/impl/SensordataApiServiceImpl.java
@@ -5,6 +5,7 @@ import io.swagger.model.*;
 
 import io.swagger.model.SensorData;
 
+import java.util.ArrayList;
 import java.util.List;
 import io.swagger.api.NotFoundException;
 
@@ -17,14 +18,26 @@ import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2019-11-21T14:54:57.056Z")
 public class SensordataApiServiceImpl extends SensordataApiService {
+	
+	private List<String[]> store = new ArrayList<String[]>();
+	
+	
+	
+	
     @Override
     public Response addSensorData(SensorData body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	
+    	String[] entry = {body.getSourceId(),body.getSensorId(),body.getUnit(),body.getValue().toString(), body.getTimestamp().toString()};
+    	
+    	store.add(entry);
+        return Response.ok().build();
     }
     @Override
     public Response getSensorData( String sourceId,  String sensorId,  Boolean latestOnly, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        System.out.println(store.get(0)[0].toString());
+        return Response.ok(store.get(0)[0].toString()).build();
+    	
+        //return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
 }
