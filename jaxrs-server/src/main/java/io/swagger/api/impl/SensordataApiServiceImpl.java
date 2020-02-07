85fd48e5492588af147cff86062370eb043cc0ce
Florian Brokhausen <florian.brokhausen@tu-berlin.de>
2020-02-07 13:36:19 +0100

    server changes to accomodate date requests

--- jaxrs-server/src/main/java/io/swagger/api/impl/SensordataApiServiceImpl.java
+++ jaxrs-server/src/main/java/io/swagger/api/impl/SensordataApiServiceImpl.java
@@ -7,6 +7,11 @@ import io.swagger.model.SensorData;
 import io.swagger.api.NotFoundException;
 import io.swagger.api.store.DataStore;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -19,7 +24,8 @@ public class SensordataApiServiceImpl extends SensordataApiService {
 	@Override
 	public Response addSensorData(SensorData body, SecurityContext securityContext) throws NotFoundException {
 
-		ds.put(body);
+//		ds.put(body);
+		ds.generateRandomData();
 
 		return Response.ok().build();
 	}
@@ -28,19 +34,51 @@ public class SensordataApiServiceImpl extends SensordataApiService {
 	public Response getSensorData(String sourceId, String sensorId, String start, String end,
 			SecurityContext securityContext) {
 
-		if (sourceId != null && sensorId != null) {
-			// get values from specific source & Sensor
-			return Response.ok(ds.getSourceSensor(sensorId, sourceId)).build();
-		} else if (sourceId == null && sensorId != null) {
-			// get values for specific sensor
-			return Response.ok(ds.getSensor(sensorId)).build();
-		} else if (sourceId != null && sensorId == null) {
-			// get values for specific source
-			return Response.ok(ds.getSource(sourceId)).build();
+		if (start != null && end != null) {
+			if (sourceId != null && sensorId != null) {
+				// get values from specific source & Sensor
+				return Response.ok(ds.getSourceSensorStartEnd(sensorId, sourceId, start, end)).build();
+			} else if (sourceId == null && sensorId != null) {
+				// get values for specific sensor
+				return Response.ok(ds.getSensorStartEnd(sensorId, start, end)).build();
+			} else if (sourceId != null && sensorId == null) {
+				// get values for specific source
+				return Response.ok(ds.getSourceStartEnd(sourceId, start, end)).build();
+			} else {
+				// get all values for all sources and sensors
+				return Response.ok(ds.getAllStartEnd(start, end)).build();
+			}
+		} else if (start != null && end == null) {
+			if (sourceId != null && sensorId != null) {
+				// get values from specific source & Sensor
+				return Response.ok(ds.getSourceSensorStart(sensorId, sourceId, start)).build();
+			} else if (sourceId == null && sensorId != null) {
+				// get values for specific sensor
+				return Response.ok(ds.getSensorStart(sensorId, start)).build();
+			} else if (sourceId != null && sensorId == null) {
+				// get values for specific source
+				return Response.ok(ds.getSourceStart(sourceId, start)).build();
+			} else {
+				// get all values for all sources and sensors
+				System.out.println("Reached");
+				return Response.ok(ds.getAllStart(start)).build();
+			}
+		} else if (start == null && end != null) {
+			return Response.ok("Wrong usage - only end date specified").build();
 		} else {
-			// get all values for all sources and sensors
-			return Response.ok(ds.getAll()).build();
+			if (sourceId != null && sensorId != null) {
+				// get values from specific source & Sensor
+				return Response.ok(ds.getSourceSensor(sensorId, sourceId)).build();
+			} else if (sourceId == null && sensorId != null) {
+				// get values for specific sensor
+				return Response.ok(ds.getSensor(sensorId)).build();
+			} else if (sourceId != null && sensorId == null) {
+				// get values for specific source
+				return Response.ok(ds.getSource(sourceId)).build();
+			} else {
+				// get all values for all sources and sensors
+				return Response.ok(ds.getAll()).build();
+			}
 		}
-
 	}
 }
--- jaxrs-server/src/main/java/io/swagger/api/store/DataStore.java
+++ jaxrs-server/src/main/java/io/swagger/api/store/DataStore.java
@@ -3,6 +3,7 @@ package io.swagger.api.store;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,7 +26,7 @@ public class DataStore {
 
 	private File csvFile;
 
-	private String dateformat = "yyyy-mm-dd hh:mm:ss";
+	public static final String dateformat = "yyyy-mm-dd'T'hh:mm:ss.sss";
 
 	public DataStore() {
 
@@ -53,8 +54,9 @@ public class DataStore {
 			sb.append(",");
 			sb.append(entry.getSensorId());
 			sb.append(",");
+			final String dateformat = "yyyy-mm-dd'T'hh:mm:ss.sss";
 			DateFormat format = new SimpleDateFormat(dateformat);
-			String strDate = format.format(entry.getTimestamp());
+			String strDate = format.format(entry.getTimestamp()) + "Z";
 			sb.append(strDate);
 			sb.append(",");
 			sb.append(entry.getUnit());
@@ -90,6 +92,49 @@ public class DataStore {
 		return entries;
 	}
 
+	public List<SensorData> getAllStart(String rawStartDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate;
+				curDate = convertDatestring(data[2]);
+				if (startDate.compareTo(curDate) < 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getAllStartEnd(String rawStartDate, String rawEndDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			Date endDate = convertDatestring(rawEndDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate;
+				curDate = convertDatestring(data[2]);
+				if (startDate.compareTo(curDate) < 0 && endDate.compareTo(curDate) > 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
 	public List<SensorData> getSource(String sourceID) {
 		String row = "";
 		List<SensorData> entries = new ArrayList<SensorData>();
@@ -99,12 +144,52 @@ public class DataStore {
 				String[] data = row.split(",");
 				if (data[0].contentEquals(sourceID)) {
 					entries.add(extractValues(data));
-				} else {
-					continue;
 				}
 			}
 			csvReader.close();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSourceStart(String sourceID, String rawStartDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[0].contentEquals(sourceID) && startDate.compareTo(curDate) < 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSourceStartEnd(String sourceID, String rawStartDate, String rawEndDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			Date endDate = convertDatestring(rawEndDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[0].contentEquals(sourceID) && startDate.compareTo(curDate) < 0
+						&& endDate.compareTo(curDate) > 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
 		return entries;
@@ -119,12 +204,52 @@ public class DataStore {
 				String[] data = row.split(",");
 				if (data[1].contentEquals(sensorID)) {
 					entries.add(extractValues(data));
-				} else {
-					continue;
 				}
 			}
 			csvReader.close();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSensorStart(String sensorID, String rawStartDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[1].contentEquals(sensorID) && startDate.compareTo(curDate) < 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSensorStartEnd(String sensorID, String rawStartDate, String rawEndDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			Date endDate = convertDatestring(rawEndDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[1].contentEquals(sensorID) && startDate.compareTo(curDate) < 0
+						&& endDate.compareTo(curDate) > 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
 		return entries;
@@ -139,12 +264,54 @@ public class DataStore {
 				String[] data = row.split(",");
 				if (data[0].contentEquals(sourceID) && data[1].contentEquals(sensorID)) {
 					entries.add(extractValues(data));
-				} else {
-					continue;
 				}
 			}
 			csvReader.close();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSourceSensorStart(String sensorID, String sourceID, String rawStartDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[0].contentEquals(sourceID) && data[1].contentEquals(sensorID)
+						&& startDate.compareTo(curDate) < 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public List<SensorData> getSourceSensorStartEnd(String sensorID, String sourceID, String rawStartDate,
+			String rawEndDate) {
+		String row = "";
+		List<SensorData> entries = new ArrayList<SensorData>();
+		try {
+			Date startDate = convertDatestring(rawStartDate);
+			Date endDate = convertDatestring(rawEndDate);
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				Date curDate = convertDatestring(data[2]);
+				if (data[0].contentEquals(sourceID) && data[1].contentEquals(sensorID)
+						&& startDate.compareTo(curDate) < 0 && endDate.compareTo(curDate) > 0) {
+					entries.add(extractValues(data));
+				}
+			}
+			csvReader.close();
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
 		return entries;
@@ -156,8 +323,7 @@ public class DataStore {
 		entry.setSensorId(data[1]);
 		Date date;
 		try {
-			DateFormat format = new SimpleDateFormat(dateformat);
-			date = format.parse(data[2]);
+			date = convertDatestring(data[2]);
 			entry.setTimestamp(date);
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -191,6 +357,56 @@ public class DataStore {
 
 		return sensors;
 
+	}
+
+	public Date getLastLoggedDate() {
+		String row = "";
+		Date date = null;
+		try {
+			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+			while ((row = csvReader.readLine()) != null) {
+				String rawDate = row.split(",")[2];
+				DateFormat format = new SimpleDateFormat(dateformat);
+				date = format.parse(rawDate);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+	public Date convertDatestring(String rawDate) throws ParseException {
+		final String dateformat = "yyyy-mm-dd'T'hh:mm:ss.sss";
+		DateFormat format = new SimpleDateFormat(dateformat);
+		System.out.println(rawDate);
+		Date date = format.parse(rawDate);
+		System.out.println(format.format(date));
+		return date;
+	}
+	
+	public void generateRandomData() {
+		double temp = Math.random() * 30;
+		for (int i = 10; i < 60; i++) {
+			String time = "2019-11-22T09:" + i + ":00.000";
+			temp = temp + Math.random() * 2 - 1;
+			SensorData entry = new SensorData();
+			entry.setSourceId("1");
+			entry.setSensorId("1");
+			Date date;
+			try {
+				date = convertDatestring(time);
+				entry.setTimestamp(date);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			entry.setUnit("C");
+			entry.setValue(temp);
+			put(entry);
+		}
 	}
 
 }
